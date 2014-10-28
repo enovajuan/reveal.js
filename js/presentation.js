e14a4250fb86cce80cf5d33e37d22b9e6505468f
Juan Bonfante <jbonfante@enova.com>
2014-10-28 18:05:18 -0500

    Project files

--- js/presentation.js
+++ js/presentation.js
@@ -1,10 +1,27 @@
-Reveal.addEventListener( 'sisteenzoom', function() {
-    zoom.to({
-        element: document.getElementById('blankZoom'),
-        scale: 2
 
-    })
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    if(event.currentSlide.dataset.state === 'sisteen')
+    {
+        zoom.to({
+            x:   getRandomInt(0, 900),
+            y: getRandomInt(0, 400),
+            scale: getRandomInt(1,9),
+
+        })
+    }
+    else
+    {
+        zoom.out();
+    }
 } );
+
+
 
 Reveal.addEventListener( 'reset', function() {
     zoom.out();
