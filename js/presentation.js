6b04d2f5ef3bbf94c3ccab8b443808aeb47cdfcf
Juan Bonfante <jbonfante@enova.com>
2014-10-30 17:07:25 -0500

    Fixed up presentation slides. Got UML diagrams to work on slide 17+18. A little bit of cleanup for used js libraries. Added SCSS and coffeescript files, as well as included copies of js libraries, so it would have no external dependencies. Added presentation images.
    Module [reveal.js]:
      + Project_Libraries.xml
      * fancy.css
      + fancy.css.map
      + fancy.scss
      + jumly.min.css
      + jumly.min.css.map
      + jumly.min.scss
      + coffee-script.js
      + jquery-2.1.1.min.js
      + jumly.min.js
      + hs-2006-01-q-full_jpg.jpg
      + hs-2006-10-a-hires_jpg.jpg
      + hs-2007-16-a-full_jpg.jpg
      + hs-2007-16-a-hires_jpg.jpg
      + hs-2009-25-a-full_jpg.jpg
      + hs-2011-11-a-full_jpg.jpg
      + hs-2012-01-g-full_jpg.jpg
      + hs-2013-12-a-full_jpg.jpg
      + hs-2013-13-a-hires_jpg.jpg
      + hs-2014-02-e-hires_jpg.jpg
      * presentation.js
      + bower.json
      * index.html
      + uml.html
      + uml2.html

--- js/presentation.js
+++ js/presentation.js
@@ -4,25 +4,28 @@ function getRandomInt(min, max) {
 }
 
 
-Reveal.addEventListener( 'slidechanged', function( event ) {
-    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-    if(event.currentSlide.dataset.state === 'sisteen')
-    {
-        zoom.to({
-            x:   getRandomInt(0, 900),
-            y: getRandomInt(0, 400),
-            scale: getRandomInt(1,9),
+    Reveal.addEventListener( 'slidechanged', function( event ) {
+        // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+        if(event.currentSlide.dataset.state === 'sisteen')
+        {
+            zoom.to({
+                x:   getRandomInt(0, 900),
+                y: getRandomInt(0, 400),
+                scale: getRandomInt(1,9),
 
-        })
-    }
-    else
-    {
+            })
+        }
+        else
+        {
+            zoom.out();
+        }
+    } );
+
+
+
+    Reveal.addEventListener( 'reset', function() {
         zoom.out();
-    }
-} );
+    } );
 
 
 
-Reveal.addEventListener( 'reset', function() {
-    zoom.out();
-} );
